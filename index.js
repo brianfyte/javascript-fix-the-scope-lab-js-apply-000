00741081c3225ed0d1904e11edb7cbf7a7bfd172
Brian Wong <brianw385@gmail.com>
2020-02-29 19:25:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,4 +31,4 @@ function funkyFunction() {
 var oh = funkyFunction();
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-oh;
+theFunk();
